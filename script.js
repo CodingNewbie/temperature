e2fd278daefcf2e6e44c4bd45156a27de2637c2e
Adam <adamrayschmidt@gmail.com>
2024-01-29 00:05:38 -0800

    Add structure, style, and JS functionality.

---
+++ script.js
@@ -0,0 +1,37 @@
+
+let unitSupF = "°F";
+let unitSupC = "°C";
+let convertBtn = document.getElementById("convert-btn");
+convertBtn.addEventListener("click", convertTemp);
+
+function convertTemp(){
+    if (document.getElementById("unit").value == "celsius"){
+        if (document.getElementById("convert-to").value == "fahrenheit"){
+            let inputTemp = document.getElementById("input-temp").value;
+            let outputTemp = ((inputTemp * (9/5) + 32)).toFixed(1);
+            document.getElementById("output").innerHTML = outputTemp;
+            document.getElementById("unit-sup").innerHTML = unitSupF;
+        }
+        else{
+            let inputTemp = document.getElementById("input-temp").value;
+            let outputTemp = inputTemp;
+            document.getElementById("output").innerHTML = outputTemp;
+            document.getElementById("unit-sup").innerHTML = unitSupC;
+        }
+    }
+    else if (document.getElementById("unit").value == "fahrenheit"){
+        if (document.getElementById("convert-to").value == "celsius"){
+            let inputTemp = document.getElementById("input-temp").value;
+            let outputTemp = ((inputTemp - 32) * 5 / 9).toFixed(1);
+            document.getElementById("output").innerHTML = outputTemp;
+            document.getElementById("unit-sup").innerHTML = unitSupC;
+        }
+        else{
+            let inputTemp = document.getElementById("input-temp").value;
+            let outputTemp = inputTemp;
+            document.getElementById("output").innerHTML = outputTemp;
+            document.getElementById("unit-sup").innerHTML = unitSupF;
+        }
+    }
+}
+
